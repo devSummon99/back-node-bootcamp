3651d654bf71082012e21f2ba9e6cf776f9d3e8d
devSummon99 <michelperezsomoza@gmail.com>
2025-04-23 17:31:01 -0400

    services notes

--- services.js
+++ services.js
@@ -1,15 +1,33 @@
-export const getAllNotesService = () => {
+import notes from "./notes.js"
 
+export const getAllNotesService = () => {
+return notes;
 }
 export const getNoteByIDService = (id) => {
-    
+const findNote = notes.find((note) => note.id = id);
+return findNote;
 }
 export const createNoteService = (data) => {
-    
+    const { title, content } = data;
+    const ids = notes.map(note => note.id);
+    const maxID = Math.max(ids);
+    const newNote = {
+        id: maxID + 1,
+        title: title,
+        content: content, 
+        date: new Date().toISOString()
+    }
+    return notes = notes.concat(newNote);
 }
-export const updateNoteService = (data) => {
+export const updateNoteService = (data, id) => {
+    const findNote = notes.find((note) => note.id = id);
+    if(findNote) {
+        Object.assign( findNote, data);
+        return findNote;
+    }else return null;
     
 }
 export const removeNoteService = (id) => {
-    
+    const deletedNote = notes.filter((note) => note.id === id)
+    return deletedNote;
 }
