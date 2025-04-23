60dd74f739de954d0d8afa0e2785420371d4f0ed
devSummon99 <michelperezsomoza@gmail.com>
2025-04-22 23:26:05 -0400

    crud

---
+++ controllers.js
@@ -0,0 +1,19 @@
+export const getAllNotes = (req, res) => {
+
+}
+
+export const getNoteByID = (req, res) => {
+    
+}
+
+export const createNote = (req, res) => {
+    
+}
+
+export const updatedNote = (req, res) => {
+    
+}
+
+export const removeNote = (req, res) => {
+    
+}
---
+++ services.js
@@ -0,0 +1,15 @@
+export const getAllNotesService = () => {
+
+}
+export const getNoteByIDService = (id) => {
+    
+}
+export const createNoteService = (data) => {
+    
+}
+export const updateNoteService = (data) => {
+    
+}
+export const removeNoteService = (id) => {
+    
+}
